87cf75b16693a11a1952c9cecf6f9e157f3c601f
Sardar <sardarhassanzada2005@gmail.com>
2026-02-19 12:11:44 -0800

    fetch and display weather data

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const clouds = document.getElementById('clouds');
 const humidity = document.getElementById('humidity');
 const pressure = document.getElementById('pressure');
 const form = document.querySelector('form');
+const main = document.querySelector('main');
+
 
 form.addEventListener('submit' ,(e)=> {
     e.preventDefault()
@@ -15,20 +17,36 @@ form.addEventListener('submit' ,(e)=> {
     }
 })
 
-let id = '';
+let id = '8cbc9650bb4b1258a753058ee6798083';
 let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid='+id;
 
-const searchWeather = () => {
-    fetch(url + '&q' + searchValue.value)
+const searchWeather = () =>{
+    fetch(url + '&q=' + searchValue.value)
     .then(responsive => responsive.json())
     .then(data => {
         console.log(data)
         if(data.cod == 200){
             city.querySelector('figcaption').innerText = data.name;
-            city.querySelector()
+            city.querySelector('img').src = 'https:flagsapi.com/'+data.sys.country+'/shiny/32.png';
+            temperature.querySelector('img').src ='https://openweathermap.org/img/wn/'+data.weather[0].icon+'@4x.png'
+            temperature.querySelector('figcaption span').innerText = data.main.temp;
+            description.innerText = data.weather[0].description;
+            clouds.innerText = data.clouds.all;
+            humidity.innerText = data.main.humidity;
+            pressure.innerText = data.main.pressure;
+        }else{
+            // false
+            main.classList.add('error');
+            setTimeout(() => {
+                main.classList.remove('error');
+            }, 1000);
         }
+        searchValue.value = '';
     })
 }
 
-
-// prevent Defalut
+const initApp = () => {
+    searchValue.value = 'Magdeburg'
+    searchWeather();
+}
+initApp()
